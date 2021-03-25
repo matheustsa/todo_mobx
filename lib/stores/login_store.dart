23fedcd89eb108a773b095e4c4b02bcc24c9a7d7
Matheus Tsa <mtsa.dev@gmail.com>
2021-03-25 20:06:53 -0300

    Update login_store.dart with observables and actions

--- lib/stores/login_store.dart
+++ lib/stores/login_store.dart
@@ -4,4 +4,22 @@ part 'login_store.g.dart';
 
 class LoginStore = _LoginStore with _$LoginStore;
 
-abstract class _LoginStore with Store {}
+abstract class _LoginStore with Store {
+  // _LoginStore(){
+  //   autorun((_){
+  //
+  //   });
+  // }
+
+  @observable
+  String email = "";
+
+  @action
+  void setEmail(String value) => email = value;
+
+  @observable
+  String password = "";
+
+  @action
+  void setPassword(String value) => password = value;
+}
